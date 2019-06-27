c4a427740cb15d4455a204be7ab8d35a6d23bdfa
SzabolcsSed <szabolcs.sed@gmail.com>
2019-06-27 12:33:02 +0200

    second bunch of excercises

---
+++ week1/day4/coding-hours.ts
@@ -0,0 +1,5 @@
+    
+'use strict';
+
+console.log(6*17*5);
+console.log(Math.round(100*5*6/52)+"%");
---
+++ week1/day4/introduce-yourself.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log("Szabolcs Sed");
+console.log(36);
+console.log(1.86);
