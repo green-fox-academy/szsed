4faac0bd946bd56fbb461b420fc803b3a9b85ab5
SzabolcsSed <szabolcs.sed@gmail.com>
2019-06-28 12:30:50 +0200

    "stringrepeat test"

---
+++ week1/day4/excercises/stringrepeat.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let space = " ";
+
+let spaces = space.repeat(15);
+
+console.log(spaces, "end");
---
+++ week1/day4/excercises/stringrepeat.ts
@@ -0,0 +1,7 @@
+'use strict'
+
+let space: string = " ";
+
+let spaces: string = space.repeat(15);
+
+console.log(spaces, "end");
