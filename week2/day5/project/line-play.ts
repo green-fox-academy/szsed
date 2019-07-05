00b2a9882ae7162d7f642158957ee24e8f3225f7
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-05 10:49:23 +0200

    drawing project implemented canvas division parameter into function

--- week2/day5/project/line-play.ts
+++ week2/day5/project/line-play.ts
@@ -9,13 +9,16 @@ let color2: string = "purple";
 
 let lineDensityParameter = 20;
 
-let canvasDivisionParameter: number = 1;
+let canvasDivisionParameter: number = 4;
+
+let widthParam: number = canvas.width/canvasDivisionParameter;
+let heightParam: number = canvas.height/canvasDivisionParameter;
 
 let drawLineAcrossTop = (startingPoint: number[], color: string) => {
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.moveTo(startingPoint[0], startingPoint[1]);
-    ctx.lineTo(canvas.width - startingPoint[1], startingPoint[0]);
+    ctx.lineTo(widthParam - startingPoint[1], startingPoint[0]);
     ctx.stroke();
 
 }
@@ -23,7 +26,7 @@ let drawLineAcrossBottom = (startingPoint: number[], color: string) => {
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.moveTo(startingPoint[0], startingPoint[1]);
-    ctx.lineTo(startingPoint[1], canvas.height - startingPoint[0]);
+    ctx.lineTo(startingPoint[1], heightParam - startingPoint[0]);
     ctx.stroke();
 
 }
@@ -36,7 +39,6 @@ let drawLines = (lDParamInner: number) => {
     }
 }
 
-// drawLines(20);
 // 1 XP Divide the canvas into 4/16/64 equal parts and repeat the line play pattern in each quarter
 
 let drawLines2 = (lDParamInner: number, cDParamInner: number) => {
@@ -44,12 +46,12 @@ let drawLines2 = (lDParamInner: number, cDParamInner: number) => {
     for (let j = 0; j < cDParamInner; j++) {
         for (let k = 0; k < cDParamInner; k++) {
 
-            // looping through canvas parts
+            // looping through canvas parts j: horizontal; k: vertical
 
             for (let i = 1; i < lDParamInner; i++) {
-                drawLineAcrossTop([i * canvas.width / lDParamInner, 0], color1);
+                drawLineAcrossTop([i * widthParam/lDParamInner + j * widthParam, k * heightParam], color1);
 
-                drawLineAcrossBottom([0, i * canvas.height / lDParamInner], color2);
+                drawLineAcrossBottom([j * widthParam, i * heightParam/lDParamInner + k * heightParam], color2);
             }
         }
     }
