30dfba434e9bf9bcc021ff8a7ae3de825ad68078
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-05 12:10:24 +0200

    drawing project triangles implemented for loop

--- week2/day5/project/triangles.ts
+++ week2/day5/project/triangles.ts
@@ -7,18 +7,23 @@ const ctx = canvas.getContext('2d');
 
 
 let startCoord: number [] = [canvas.width/2, 0];
-let numberOfRows: number = 1;
+let numberOfRows: number = 10;
 let sideLength: number = 20;
+let triangleHeight: number = (3**0.5)/2* sideLength;
 
 let drawTriangle = (coordInner: number[]) =>{
 
     ctx.beginPath();
     ctx.moveTo(coordInner[0], coordInner[1]);
-    ctx.lineTo(coordInner[0]+ sideLength/2, coordInner[1]+(3**0.5)/2* sideLength);
-    ctx.lineTo(coordInner[0]- sideLength/2, coordInner[1]+(3**0.5)/2* sideLength);
+    ctx.lineTo(coordInner[0]+ sideLength/2, coordInner[1]+triangleHeight;
+    ctx.lineTo(coordInner[0]- sideLength/2, coordInner[1]+triangleHeight);
     ctx.closePath();
     ctx.stroke();
 
 }
 
-drawTriangle(startCoord);
+for (let i = 0; i < numberOfRows; i++){
+    for (let j = 0; j < i+1; j++){
+        drawTriangle([startCoord[0] - i*sideLength/2 + j* sideLength, startCoord[1] + i*triangleHeight]);
+    }
+}
