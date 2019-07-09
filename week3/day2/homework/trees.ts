5cb282fc05be57b4ab6fa97074ef5e010eead87d
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-09 22:27:46 +0200

    w3d2 homework tree objects

---
+++ week3/day2/homework/trees.ts
@@ -0,0 +1,42 @@
+// # Doable homework
+// - Create 5 trees
+//   - Store the data of them in variables in your program
+//   - for every tree the program should store its'
+//     - type
+//     - leaf color
+//     - age
+//     - sex
+//   - you can use just variables, or lists and/or maps
+
+'use strict'
+
+let treebeard = {
+'type' : 'ent',
+'leaf color' : 'green',
+'age' : 14674,
+'sex' : 'male'
+};
+let grandmotherWillow = {
+'type' : 'willow',
+'leaf color' : 'blue',
+'age' : 145,
+'sex' : 'female'
+};
+let toadTree = {
+'type' : 'oak',
+'leaf color' : 'none',
+'age' : 3843,
+'sex' : 'none'
+};
+let theWhompingWillow = {
+'type' : 'willow',
+'leaf color' : 'green',
+'age' : 48,
+'sex' : 'unknown'
+};
+let groot = {
+'type' : 'superhero',
+'leaf color' : 'green',
+'age' : 55,
+'sex' : 'male'
+};
