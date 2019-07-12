78b18761a37c6641560c8c13fefb5701dce48af7
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-12 11:13:29 +0200

    w3d5 excercises number adder

---
+++ week3/day5/excercises/numberadder.ts
@@ -0,0 +1,29 @@
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+'use strict';
+
+let numberAdder = (n: number): number =>{
+
+    console.log(n);
+
+try {
+    if (n < 1){
+        throw Error('invalid input parameter: please provide an integer greater than 0');
+        }
+} catch (error) {
+    console.log(error.message);
+    return undefined;
+}
+
+    let result: number = 0;
+    
+    if (n == 1){
+        result++;
+        return result;
+    }
+    result += n + numberAdder(n-1);
+    return result;
+}
+
+console.log(numberAdder(-21));
+console.log(numberAdder(5));
