16e9e92bf0374735d2e31cbf584b52b6528ed615
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-18 21:43:23 +0200

    w4d4 excercises cows and bulls constructor tests

---
+++ week4/day4/excercises/cows-and-bulls/test.ts
@@ -0,0 +1,35 @@
+'use strict';
+
+import test from 'tape';
+import CowsAndBulls from './cows-and-bulls';
+
+test('testing CowsAndBulls object constructor', (t: any) => {
+    let testCAB = new CowsAndBulls();
+    t.equals(testCAB.getTarget().toString().length, 4);
+    t.equals(testCAB.getGameState(), 'playing');
+    t.equals(testCAB.getCounter(), 0);
+    t.equals(testCAB.constructor.name, 'CowsAndBulls');
+    t.end();
+});
+
+test('testing guess method', (t: any) => {
+    let testCAB = new CowsAndBulls();
+    testCAB.setTarget(4567);
+    t.equals(testCAB.getTarget(), 4567);  
+    t.notEquals(testCAB.guess(1234), '4 cows');
+    t.equals(testCAB.guess(1234), '1 bull');
+    t.equals(testCAB.getCounter(), 1);
+    t.equals(testCAB.getGameState(), 'playing');
+    t.end();
+});
+
+test('testing game end', (t: any) => {
+    let testCAB = new CowsAndBulls();
+    testCAB.setTarget(4567);
+    t.equals(testCAB.guess(4567), '4 cows');
+    t.equals(testCAB.getCounter(), 1);
+    t.equals(testCAB.getGameState(), 'finished');
+    t.equals(testCAB.guess(1234), 'game over');
+    t.equals(testCAB.getCounter(), 1);
+    t.end();
+});
