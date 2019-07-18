2c871ad3bbfd963058fbab318db650da615b28b9
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-18 22:58:03 +0200

    w3d5 excercises koch line first layer

--- week3/day5/excercises/fractals/koch-line.ts
+++ week3/day5/excercises/fractals/koch-line.ts
@@ -6,29 +6,38 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 const numberOfLayersKoch: number = 3;
-const startCoords: [[number, number],[number, number]] = [[0, canvas.width / 2], [canvas.width, canvas.width / 2]];
+const startCoordsKoch: [[number, number], [number, number]] = [[0, canvas.height / 2], [canvas.width, canvas.height / 2]];
 
-let calcNextLayerCoords = (inputCoords: [number, number][], inputSize: number): [number, number][] => {
+let coords: [[number, number], [number, number]] = [];
+
+
+let calcNextLayerCoords = (inputCoords: [number, number][]): [number, number][] => {
+    let inputSize: number = Math.sqrt(Math.pow((inputCoords[0][0] - inputCoords[1][0]), 2));
     let output: [number, number][] = [];
-    output.push([inputCoord[0], inputCoord[1]]);
-    output.push([inputCoord[0] + inputSize / 2, inputCoord[1]]);
-    output.push([inputCoord[0] + inputSize / 4, inputCoord[1] + inputSize * 3 ** 0.5 / 4]);
+    output.push([inputCoords[0][0] + inputSize / 3, inputCoords[0][1]]);
+    output.push([inputCoords[0][0] + inputSize / 2, inputCoords[0][1] - inputSize * 3 ** 0.5 / 6]);
+    output.push([inputCoords[0][0] + inputSize * 2 / 3, inputCoords[0][1]]);
     return output;
 }
 
-let drawKochLine = (startCoord: [[number, number],[number, number]], layers: number): void => {
+let drawKochLine = (coords: [[number, number], [number, number]]): void => {
 
-    // console.log('Center coordinate:', startCoord, 'Layers:', layers, 'Size:', size);
+    ctx.beginPath();
+    ctx.moveTo(coords[0][0], coords[0][1]);
 
-    if (layers > 0) {
-        ctx.beginPath();
-        ctx.moveTo(startCoord[0], startCoord[1]);
-        ctx.lineTo(startCoord[0] + size, startCoord[1]);
-        ctx.lineTo(startCoord[0] + size / 2, startCoord[1] + size * 3 ** 0.5 / 2);
-        ctx.closePath();
-        ctx.stroke();
-        calcNextToplefts(startCoord, size).forEach((element) => drawTriangleFractal(element, layers - 1, size / 2));
+    for (let i: number = 1; i < coords.length; i++) {
+        ctx.lineTo(coords[i][0], coords[i][1]);
     }
+
+    ctx.stroke();
 }
 
-drawTriangleFractal(startCoordTriangles, numberOfLayersTriangles, triangleSize);
+
+// drawKochLine(startCoordsKoch, numberOfLayersKoch);
+
+coords.push(startCoordsKoch[0]);
+console.log(calcNextLayerCoords(startCoordsKoch));
+calcNextLayerCoords(startCoordsKoch).forEach(element => coords.push(element));
+coords.push(startCoordsKoch[1]);
+
+drawKochLine(coords);
