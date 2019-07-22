4fb53d221e9232825a6d6d4955495078c6717aef
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-22 20:45:32 +0200

    w3d5 excercises koch line working version

--- week3/day5/excercises/fractals/koch-line.ts
+++ week3/day5/excercises/fractals/koch-line.ts
@@ -6,21 +6,43 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 const numberOfLayersKoch: number = 3;
-const startCoordsKoch: [[number, number], [number, number]] = [[0, canvas.height / 2], [canvas.width, canvas.height / 2]];
+const startCoordsKoch: [number, number] = [0, canvas.height / 2];
+const EndCoordsKoch: [number, number] = [canvas.width, canvas.height / 2];
+const startAngle: number = 0;
+const startSize: number = canvas.width;
+const startParamsKoch: [[number, number], number, number] = [startCoordsKoch, startAngle, startSize];
+const sixtyDegs: number = Math.PI / 3;
 
-let coords: [[number, number], [number, number]] = [];
+let paramsArr: [[number, number], number, number][] = [];
 
-
-let calcNextLayerCoords = (inputCoords: [number, number][]): [number, number][] => {
-    let inputSize: number = Math.sqrt(Math.pow((inputCoords[0][0] - inputCoords[1][0]), 2));
-    let output: [number, number][] = [];
-    output.push([inputCoords[0][0] + inputSize / 3, inputCoords[0][1]]);
-    output.push([inputCoords[0][0] + inputSize / 2, inputCoords[0][1] - inputSize * 3 ** 0.5 / 6]);
-    output.push([inputCoords[0][0] + inputSize * 2 / 3, inputCoords[0][1]]);
+let calcNextLayerParams = (inputParams: [[number, number], number, number]): [[number, number], number, number][] => {
+    let inputCoords: [number, number] = inputParams[0];
+    let inputAngle: number = inputParams[1];
+    let inputSize: number = inputParams[2];
+    let output: [[number, number], number, number][] = []
+    output.push([inputCoords, inputAngle, inputSize / 3]);
+    output.push([[inputCoords[0] + inputSize / 3 * Math.cos(inputAngle), inputCoords[1] + inputSize / 3 * Math.sin(inputAngle)], inputAngle - sixtyDegs, inputSize / 3]);
+    output.push([[inputCoords[0] + inputSize / 3 * Math.cos(inputAngle) + inputSize / 3 * Math.cos(inputAngle - sixtyDegs), inputCoords[1] + inputSize / 3 * Math.sin(inputAngle) + inputSize / 3 * Math.sin(inputAngle - sixtyDegs)], inputAngle + sixtyDegs, inputSize / 3]);
+    output.push([[inputCoords[0] + inputSize / 3 * Math.cos(inputAngle) * 2, inputCoords[1] + inputSize / 3 * Math.sin(inputAngle) * 2], inputAngle, inputSize / 3]);
     return output;
 }
 
-let drawKochLine = (coords: [[number, number], [number, number]]): void => {
+let calcAllParams = (layers: number, params: [[number, number], number, number]){
+    if (layers > 0) {
+        calcNextLayerParams(params).forEach(element => {
+            paramsArr.push(element);
+            calcAllParams(layers - 1, element)
+        });
+    }
+}
+
+calcAllParams(numberOfLayersKoch, startParamsKoch);
+let coords: [number, number][] = [];
+paramsArr.forEach(element => coords.push(element[0]));
+console.log(coords.length);
+
+
+let drawKochLine = (coords: [number, number][]): void => {
 
     ctx.beginPath();
     ctx.moveTo(coords[0][0], coords[0][1]);
@@ -29,15 +51,10 @@ let drawKochLine = (coords: [[number, number], [number, number]]): void => {
         ctx.lineTo(coords[i][0], coords[i][1]);
     }
 
+    ctx.lineTo(EndCoordsKoch[0], EndCoordsKoch[1]);
+
     ctx.stroke();
 }
 
-
-// drawKochLine(startCoordsKoch, numberOfLayersKoch);
-
-coords.push(startCoordsKoch[0]);
-console.log(calcNextLayerCoords(startCoordsKoch));
-calcNextLayerCoords(startCoordsKoch).forEach(element => coords.push(element));
-coords.push(startCoordsKoch[1]);
 
 drawKochLine(coords);
