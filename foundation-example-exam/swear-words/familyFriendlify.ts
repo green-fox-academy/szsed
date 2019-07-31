bdab261cb630183c839b5581f31418a07a713f70
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-31 16:48:50 +0200

    w5d3 foundation example exam swear words

--- foundation-example-exam/swear-words/familyFriendlify.ts
+++ foundation-example-exam/swear-words/familyFriendlify.ts
@@ -1,7 +1,31 @@
 'use strict';
 
+const fs = require('fs');
+
 const familyFriendlify = (text: string, swearwords: string[]): number => {
+    if (text == undefined) {
+        return undefined;
+    }
+
     let wordsRemoved: number = 0;
+    let wordsArray: string[] = text.split(' ');
+
+    for (let i: number = wordsArray.length - 1; i >= 0; i--) {
+        swearwords.forEach((word) => {
+            if (wordsArray[i].indexOf(word) >= 0) {
+                wordsRemoved++;
+                wordsArray.splice(i, 1);
+            }
+        })
+    }
+
+    let niceText: string = wordsArray.join(' ');
+
+    try {
+        fs.writeFileSync('nicecontent.txt', niceText)
+    } catch (error) {
+        console.log(error.message);
+    }
 
     return wordsRemoved;
 
