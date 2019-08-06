57fbb96a67e14fee7635b907f5f2993aa969e0ea
SzabolcsSed <szabolcs.sed@gmail.com>
2019-08-06 17:35:53 +0200

    w6d2 excercises last manager unfinished version

---
+++ week6/day2/excercises/list-manager/main.js
@@ -0,0 +1,76 @@
+'use strict';
+
+// 1, Build up the necessary structure with JavaScript (try to use semantically correct elements)
+// 2, Achive the same design by css
+// 3, Try not to use css classes
+// 4, Add some functionality to the buttons
+//     - If "Up" is clicked the selection should move up by one
+//     - If "Down" is clicked the selection should move down by one
+//     - If "X" is clicked the selected item should be removed and the first item should be selected
+//     - If ">" is clicked the selected item should be moved to the right side and the first item on the left side should be selected
+// 5, Check all the edge cases, no error should be printed to the console
+
+let foodlist = ['bread', 'milk', 'orange', 'tomato']
+let buttonLabels = ['Up', '>', 'X', 'Down']
+
+let container = document.createElement('div');
+container.style.display = 'flex';
+container.style.justifyContent = 'space-evenly';
+document.body.appendChild(container);
+
+for (let i = 0; i < 3; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.style.width = '100px';
+    newDiv.style.height = '100px';
+    newDiv.style.border = '1px solid black';
+
+    for (let j = 0; j < 4; j++) {
+
+        switch (i) {
+            case 0:
+                let newParag = document.createElement('p');
+                newParag.textContent = foodlist[j];
+                newParag.style.margin = '0';
+                newParag.style.textAlign = 'center';
+                newParag.style.height = '25%';
+                newParag.style.width = '100%';
+                newParag.style.display = 'block';
+                if (j === 0) newParag.style.backgroundColor = '#d3d3d3';
+                newDiv.appendChild(newParag);
+                break;
+            case 1:
+                let newButton = document.createElement('button');
+                newButton.textContent = buttonLabels[j];
+                newButton.style.height = '25%';
+                newButton.style.width = '100%';
+                newButton.style.display = 'block';
+                newDiv.appendChild(newButton);
+                break;
+        }
+    }
+
+    container.appendChild(newDiv);
+}
+
+let foodOnLeft = container.children[0]
+let buttons = container.children[1].children;
+
+console.log(foodOnLeft.children);
+
+buttons[0].onclick = () => {
+    Array.from(foodOnLeft.children).forEach((element, index, arr) => {
+        if (element.style.backgroundColor === 'rgb(211, 211, 211)' && index > 0) {
+            element.style.backgroundColor = '#fff';
+            arr[index - 1].style.backgroundColor = '#d3d3d3';
+        }
+    })
+}
+
+buttons[3].onclick = () => {
+    Array.from(foodOnLeft.children).forEach((element, index, arr) => {
+        if (element.style.backgroundColor === 'rgb(211, 211, 211)' && index < 3) {
+            element.style.backgroundColor = '#fff';
+            arr[index + 1].style.backgroundColor = '#d3d3d3';
+        }
+    })
+}
