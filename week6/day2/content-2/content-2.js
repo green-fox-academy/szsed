a17d9b40745153bf97d7110dc18ca10fcdef7c40
SzabolcsSed <szabolcs.sed@gmail.com>
2019-08-06 14:25:22 +0200

    w6d2 excercises content 2

---
+++ week6/day2/content-2/content-2.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// 1) replace the list items' content with items from this list:
+// ['apple', 'banana', 'cat', 'dog']
+
+let contentList = ['apple', 'banana', 'cat', 'dog'];
+Array.from(document.getElementsByTagName('li')).forEach((element, index) => element.textContent = contentList[index]);
+
+// 2) change the < ul > element's background color to 'limegreen'
+//     - use css class to change the color instead of the style property
+
+document.querySelector('ul').className = 'coloredUl';
