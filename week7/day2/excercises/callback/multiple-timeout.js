8422025880719af7de3d30dc54bd2c07f8655755
SzabolcsSed <szabolcs.sed@gmail.com>
2019-08-13 12:52:17 +0200

    w7d2 excercises up to prime squares

---
+++ week7/day2/excercises/callback/multiple-timeout.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Write a program that prints the following fruits:
+// apple -> immediately
+// pear -> after 1 seconds
+// melon -> after 3 seconds
+// grapes -> after 5 seconds
+console.log('apple');
+setTimeout(console.log, 1000, 'pear');
+setTimeout(console.log, 3000, 'melon');
+setTimeout(console.log, 5000, 'grapes');
