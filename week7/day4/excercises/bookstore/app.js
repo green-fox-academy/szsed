86e39e630988c11a41b07ee3afd1bfe5986d4b38
SzabolcsSed <szabolcs.sed@gmail.com>
2019-08-15 13:13:24 +0200

    w7d4 excercises bookstore titles

---
+++ week7/day4/excercises/bookstore/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const express = require('express');
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'asdasd',
+    database: 'bookstore'
+});
+
+conn.connect((err) => {
+    if (err) {
+        console.log('Error connecting to DB');
+        console.log(err.message);
+        return;
+    }
+    console.log('DB connection established');
+});
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static('views'));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+
+app.get('/titles', (req, res) => {
+    conn.query('select book_name from book_mast;', function (err, rows) {
+        if (err) {
+            console.log(err.message);
+        }
+
+        console.log('Data received from DB.');
+        res.render('titles', { titles: rows });
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port# ${PORT}`);
+});
