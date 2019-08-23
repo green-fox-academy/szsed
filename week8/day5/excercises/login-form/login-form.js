cf93e7a2f48cf8661c039ba4f9dfbc3d4ed53a83
SzabolcsSed <szabolcs.sed@gmail.com>
2019-08-23 12:43:46 +0200

    w8d5 excercises login form minimal css tyvm

---
+++ week8/day5/excercises/login-form/login-form.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const icon = document.querySelector('i');
+const passwordField = document.querySelector('input[type="password"]');
+
+icon.addEventListener('click', () => {
+    icon.classList.toggle('fa-eye');
+    icon.classList.toggle('fa-eye-slash');
+    passwordField.getAttribute('type') === 'password' ?
+        passwordField.setAttribute('type', 'text') :
+        passwordField.setAttribute('type', 'password');
+})
+
+console.log(passwordField);
