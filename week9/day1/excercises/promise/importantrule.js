07e66d76773772043f1f2c734166a6f3e1e40d31
SzabolcsSed <szabolcs.sed@gmail.com>
2019-09-02 12:56:25 +0200

    w9d1 excercises promise-it-wont-hurt important rule

--- week9/day1/excercises/promise/importantrule.js
+++ week9/day1/excercises/promise/importantrule.js
@@ -1,3 +1,24 @@
 'use strict';
 
-const alwaysThrows
+const alwaysThrows = () => {
+    throw 'OH NOES';
+}
+
+const iterate = int => {
+    console.log(int);
+    return ++int;
+}
+
+Promise.resolve(1)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(alwaysThrows)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .catch(console.log);
