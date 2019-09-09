5aeec93ff9094562b891528d17190b2a79f8dd5c
SzabolcsSed <szabolcs.sed@gmail.com>
2019-09-09 15:28:01 +0200

    audio player project nouislider attempt

--- week10/music-player/assets/js/audiotest.js
+++ week10/music-player/assets/js/audiotest.js
@@ -1,10 +1,44 @@
 'use strict';
 
-console.log('hello');
-
 const audio = document.querySelector('audio');
+const duration = document.querySelector('.duration');
+const remaining = document.querySelector('.remaining');
 
 audio.addEventListener('loadstart', () => console.log('loadstart happened'));
 audio.addEventListener('play', () => console.log('play happened'));
 audio.addEventListener('ended', () => console.log('ended happened'));
 audio.addEventListener('progress', () => console.log('progress happened'));
+
+const progressSlider = document.querySelector('.progress.slider');
+const volumeSlider = document.querySelector('.volume.slider');
+
+noUiSlider.create(progressSlider, {
+  start: 0,
+  range: {
+    min: 0,
+    max: audio.duration
+  },
+});
+
+noUiSlider.create(volumeSlider, {
+  start: 0,
+  range: {
+    min: 0,
+    max: 100
+  },
+});
+
+const displayRemaining = () => remaining.textContent = Math.floor((audio.duration - audio.currentTime) * 10) / 10;
+
+audio.addEventListener('loadedmetadata', () => {
+  duration.textContent = Math.floor(audio.duration * 10) / 10;
+  displayRemaining();
+});
+
+progressSlider.noUiSlider.on('change', () => {
+  audio.currentTime = progressSlider.noUiSlider.get();
+  displayRemaining();
+});
+
+const addPlaylistButton = document.querySelector('addplaylist');
+const addTrackButton = document.querySelector('addtrack');
